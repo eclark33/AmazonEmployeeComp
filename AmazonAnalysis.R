cb8adf70263883042aa11300c957d8a4366078e5
eclark33 <eliseclark13@gmail.com>
2025-10-06 12:55:32 -0600

    Update AmazonAnalysis.R

--- AmazonAnalysis.R
+++ AmazonAnalysis.R
@@ -1,7 +1,28 @@
+########################################
 ## AMAZON EMPLOYEE ACCESS PREDICTIONS ##
+########################################
+
+# libraries
+library(tidymodels)
+library(tidyverse)
+library(vroom)
 
 
+# read in data
+train_data <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/amazon-employee-access-challenge/train.csv")
 
+
+###### EDA ######
+ggplot(data = train_data, aes(x = ACTION)) + 
+  geom_bar(fill = "plum3") + 
+  xlab("Action") + ylab("Frequency") + 
+  ggtitle("Amazon Employee Access Counts")
+
+
+table(train_data$ACTION, train_data$ROLE_FAMILY)
+
+
+# 1050 is correct!
 
 
 
