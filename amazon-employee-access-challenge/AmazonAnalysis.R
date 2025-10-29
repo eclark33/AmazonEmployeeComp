ade6776e1955ef9aa16211e942b03068e9ea2a9d
eclark33 <eliseclark13@gmail.com>
2025-10-29 17:44:52 -0600

    Added SVM models

--- amazon-employee-access-challenge/AmazonAnalysis.R
+++ amazon-employee-access-challenge/AmazonAnalysis.R
@@ -10,6 +10,16 @@ library(glmnet)
 library(embed)
 library(janitor)
 library(kknn)
+library(discrim)
+library(naivebayes)
+library(keras)
+library(tune)
+library(tictoc)
+library(kernlab)
+
+
+library(tensorflow)
+
 
 # read in data
 train_data <- vroom("/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/amazon-employee-access-challenge/train.csv")
@@ -21,6 +31,7 @@ testData <- clean_names(testData)
 
 train_data <- train_data %>% 
   mutate(action = as.factor(action))
+
 
 ###### EDA ######
 ggplot(data = train_data, aes(x = action)) + 
@@ -39,7 +50,9 @@ my_recipe <- recipe(action ~ . , data = train_data) %>%
   step_mutate_at(c(resource, mgr_id, role_rollup_1, role_rollup_2, role_deptname, 
                    role_title, role_family_desc, role_family, role_code), fn = factor) %>% 
   step_other(all_nominal_predictors(), threshold = .001) %>% # combines categorical values that occur <.1% 
-  step_dummy(all_nominal_predictors())
+  step_dummy(all_nominal_predictors()) %>%
+  step_normalize(all_nominal_predictors()) %>%
+                   step_pca(all_predictors(), threshold = 0.85)
 
 
 prep <- prep(my_recipe)
@@ -74,7 +87,7 @@ amazon_preds <- amazon_preds %>%
   rename(action = .pred_1)   
 
 
-
+vroom_write(x = amazon_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/pca1.csv", delim = ",")
 
 
 ###### PENALIZED LOGISTIC REGRESSION ######
@@ -85,12 +98,13 @@ penlog_recipe <- recipe(action ~ ., data = train_data) %>%
                    role_deptname, role_title, role_family_desc, 
                    role_family, role_code), fn = factor) %>%
   step_other(all_nominal_predictors(), threshold = 0.001) %>%
-  step_lencode_mixed(c(resource, mgr_id, role_rollup_1, role_rollup_2, 
+  step_lencode_glm(c(resource, mgr_id, role_rollup_1, role_rollup_2, 
                        role_deptname, role_title, role_family_desc, 
                        role_family, role_code), outcome = vars(action)) %>%
   step_rm(mgr_id) %>%
   step_zv(all_predictors()) %>%
-  step_normalize(all_numeric_predictors())
+  step_normalize(all_numeric_predictors()) %>%
+  step_pca(all_predictors())
 
 
 #target encoding (must be 2-f8
@@ -111,13 +125,14 @@ tuning_grid <- grid_regular(penalty(),
                               mixture(),
                               levels = 4) ## L^2 total tuning possibilities
 # Split data for CV
-folds <- vfold_cv(train_data, v = 5, repeats = 3)
+folds <- vfold_cv(train_data, v = 3, repeats = 2)
 
 # Run the CV
 CV_results <- penlog_workflow %>%
  tune_grid(resamples = folds,        # Mcallentexas2324!
             grid = tuning_grid,
             metrics = metric_set(roc_auc))
+
 
 # Find best tuning parameters
 bestTune <- CV_results %>%
@@ -140,8 +155,7 @@ penlog_preds <- penlog_preds %>%
 
 
 # kaggle files
-vroom_write(x = amazon_preds, file = "batch0.csv", delim = ",")
-vroom_write(x = penlog_preds, file = "batch.csv", delim = ",")
+vroom_write(x = penlog_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/pca2.csv", delim = ",")
 
 
 
@@ -151,14 +165,12 @@ vroom_write(x = penlog_preds, file = "batch.csv", delim = ",")
 forest_recipe <- recipe(action ~ ., data = train_data) %>%
   step_mutate_at(c(resource, mgr_id, role_rollup_1, role_rollup_2, 
                    role_deptname, role_title, role_family_desc, 
-                   role_family, role_code), fn = factor) %>%
+                   role_family, role_code), fn = factor) %>% 
   step_other(all_nominal_predictors(), threshold = 0.001) %>%
-  step_lencode_mixed(c(resource, mgr_id, role_rollup_1, role_rollup_2, 
-                       role_deptname, role_title, role_family_desc, 
-                       role_family, role_code), outcome = vars(action)) %>%
-  step_rm(mgr_id) %>%
-  step_zv(all_predictors()) %>%
-  step_normalize(all_numeric_predictors())
+  step_lencode_glm(all_nominal_predictors(), outcome = vars(action)) %>%
+  step_interact(terms = ~ resource:role_deptname + resource:role_title + mgr_id:role_family) %>%  # added interaction
+  step_zv(all_predictors())
+
 
 forest_prep <- prep(forest_recipe)
 forest_data <- bake(forest_prep, new_data = train_data)
@@ -166,10 +178,10 @@ forest_data <- bake(forest_prep, new_data = train_data)
 
 # model 
 forest_lm <- rand_forest(mtry = tune(),
-                      min_n = tune(),
-                      trees = 500) %>%
-  set_engine("ranger") %>%
-  set_mode("classification")
+            min_n = tune(),
+            trees = tune()) %>%
+            set_engine("ranger") %>%
+            set_mode("classification")
 
 
 # workflow
@@ -177,17 +189,23 @@ forest_wf <- workflow() %>%
   add_recipe(forest_recipe) %>% add_model(forest_lm) 
 
 # grid of tuning values
-tuning_grid <- grid_regular(mtry(range = c(1, 8)),
-                            min_n(range = c(2, 10)),
-                            levels = 5)
+tuning_grid <- grid_random(
+  mtry(range = c(1, floor(sqrt(ncol(train_data))))),
+  min_n(range = c(2, 15)),
+  trees(range = c(500, 1500)),
+  size = 50)
 
 # set up K-fold CV
-folds <- vfold_cv(train_data, v = 5, repeats = 3)
+folds <- vfold_cv(train_data, v = 5, repeats = 2)
 
+tic()
 CV_results <- forest_wf %>%
-  tune_grid(resamples = folds,        
-            grid = tuning_grid,
-            metrics = metric_set(roc_auc))
+  tune_grid(
+    resamples = folds,        
+    grid = tuning_grid,
+    metrics = metric_set(roc_auc),
+    control = control_grid(verbose = TRUE))
+toc()
 
 # find best tuning parameters
 bestTune <- CV_results %>%
@@ -207,7 +225,7 @@ forest_preds <- forest_preds %>%
   select(id, .pred_1) %>%        
   rename(action = .pred_1)  
 
-vroom_write(x = forest_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/forest_preds.csv", delim = ",")
+vroom_write(x = forest_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/preds_uno.csv", delim = ",")
 
 
 
@@ -219,7 +237,8 @@ knn_recipe <- recipe(action ~ ., data = train_data) %>%
   step_other(all_nominal_predictors(), threshold = 0.001) %>%
   step_lencode_mixed(all_nominal_predictors(), outcome = vars(action)) %>%
   step_zv(all_predictors()) %>%
-  step_normalize(all_numeric_predictors())
+  step_normalize(all_numeric_predictors()) %>%
+  step_pca(all_predictors())
 
 # knn model
 knn_model <- nearest_neighbor(neighbors = tune()) %>% # set or tune
@@ -262,15 +281,195 @@ knn_preds <- knn_preds %>%
   select(id, .pred_1) %>%        
   rename(action = .pred_1)  
 
-vroom_write(x = forest_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/knn_preds.csv", delim = ",")
+vroom_write(x = knn_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/knn_preds2.csv", delim = ",")
+
+
+
+############ NAIVE BAYES ############
+
+# recipe
+nb_recipe <- recipe(action ~ ., data = train_data) %>%
+  step_rm(mgr_id) %>%
+  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(action)) %>%
+  step_zv(all_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+
+## nb model
+nb_model <- naive_Bayes(Laplace = tune(), smoothness = tune()) %>%
+  set_mode("classification") %>%
+  set_engine("naivebayes")
+
+# workflow 
+nb_wf <- workflow() %>%
+  add_recipe(nb_recipe) %>%
+  add_model(nb_model)
+
+# Tune smoothness and Laplace here
+# grid of tuning values
+tuning_grid <- grid_regular(Laplace(range = c(0,2)), 
+                            smoothness(range = c(0,2)),
+                            levels = 5)
+
+# set up K-fold CV
+folds <- vfold_cv(train_data, v = 3, repeats = 2)
+
+CV_results <- nb_wf %>%
+  tune_grid(resamples = folds,        
+            grid = tuning_grid,
+            metrics = metric_set(roc_auc))
+
+# find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "roc_auc")
+
+# finalize workflow and predict
+final_wf <- nb_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = train_data)
+
+
+# Predict
+nb_preds <- final_wf %>%
+  predict(new_data = testData, type = "prob")
+
+nb_preds <- nb_preds %>%
+  mutate(id = row_number()) %>%
+  select(id, .pred_1) %>%        
+  rename(action = .pred_1) 
+
+vroom_write(x = nb_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/nb_preds.csv", delim = ",")
+
+# above 0.885
+
+
+########## NEURAL NETWORK ##########
+py_require("tensorflow")
+
+nn_recipe <- recipe(action ~ ., data = train_data) %>%
+  step_dummy(all_nominal_predictors()) %>%
+  step_range(all_numeric_predictors(), min = 0, max = 1)
+
+nn_model <- mlp(hidden_units = tune(),
+                epochs = 50) %>% 
+  set_engine("keras") %>% 
+  set_mode("classification")
+
+nn_wf <- workflow() %>%
+  add_model(nn_model) %>%
+  add_recipe(nn_recipe)
+                
+nn_tuneGrid <- grid_regular(hidden_units(range = c(1, 50)),
+                            levels = 2)
+
+
+# set up K-fold CV
+folds <- vfold_cv(train_data, v = 3, repeats = 2)
+
+CV_results <- nn_wf %>%
+  tune_grid(resamples = folds,        
+            grid = nn_tuneGrid,
+            metrics = metric_set(roc_auc))
+
+# find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "roc_auc")
+
+# finalize workflow and predict
+final_wf <- nn_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = train_data)
+
+# Predict
+nn_preds <- final_wf %>%
+  predict(new_data = testData, type = "prob")
+
+nn_preds <- nn_preds %>%
+  mutate(id = row_number()) %>%
+  select(id, .pred_1) %>%        
+  rename(action = .pred_1) 
+
+vroom_write(x = nn_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/nn_preds.csv", delim = ",")
+
+
+################# SUPPORT VECTOR MACHINES ##################
+### RADIAL MODEL INCLUDES SMOTE ###
+
+svm_recipe <- recipe(action ~ ., data = train_data) %>%
+  step_rm(mgr_id) %>%
+  step_other(all_nominal_predictors(), threshold = 0.001) %>%
+  step_lencode_mixed(all_nominal_predictors(), outcome = vars(action)) %>%
+  step_zv(all_predictors()) %>%
+  step_normalize(all_numeric_predictors())
+
+
+svm_prep <- prep(svm_recipe)
+svm_data <- bake(svm_prep, new_data = train_data)
+
+## SVM models
+svmPoly <- svm_poly(degree = tune(), 
+                    cost = tune()) %>% # set or tune
+                    set_mode("classification") %>%
+                    set_engine("kernlab", maxiter = 5000)
+
+# note that svmRadial worked best
+svmRadial <- svm_rbf(rbf_sigma = tune(), cost = tune()) %>% # set or tune
+              set_mode("classification") %>%
+                set_engine("kernlab")
+
+svmLinear <- svm_linear(cost=tune()) %>% # set or tune
+                set_mode("classification") %>%
+                  set_engine("kernlab")
+
+# workflow 
+svm_wf <- workflow() %>%
+  add_recipe(svm_recipe) %>%
+  add_model(svmLinear)
+
+
+# grid of tuning values
+tuning_grid <- grid_regular(
+  cost(range = c(-2, 1), trans = log10_trans()),        
+  levels = 4)
+
+# set up K-fold CV
+folds <- vfold_cv(train_data, v = 3)
+
+
+CV_results <- svm_wf %>%
+  tune_grid(
+    resamples = folds,        
+    grid = tuning_grid,
+    metrics = metric_set(roc_auc),
+    control = control_grid(verbose = TRUE))
+
+# find best tuning parameters
+bestTune <- CV_results %>%
+  select_best(metric = "roc_auc")
+
+# finalize workflow and predict
+final_wf <- svm_wf %>%
+  finalize_workflow(bestTune) %>%
+  fit(data = train_data)
+
+
+# predict
+svm_preds <- final_wf %>%
+  predict(new_data = testData, type = "prob")
+
+svm_preds <- svm_preds %>%
+  mutate(id = row_number()) %>%
+  select(id, .pred_1) %>%        
+  rename(action = .pred_1) 
+
+
+vroom_write(x = svm_preds, file = "/Users/eliseclark/Documents/Fall 2025/Stat 348/AmazonEmployeeComp/svmLinear_preds.csv", delim = ",")
 
 
 
 
-
-
-
-
+  
 
 
 
